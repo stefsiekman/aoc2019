21a9cf0341d7e24f8a0d202fab2482d1859be8e7
Stef Siekman <s.siekman@gmail.com>
2019-12-02 06:18:08 +0100

    Second part of today

--- 02/second.py
+++ 02/second.py
@@ -1,7 +1,40 @@
 import aoc.input
-import first
+
+
+def run(cells, noun, verb):
+    pointer = 0
+    cells[1] = noun
+    cells[2] = verb
+
+    while True:
+        op = cells[pointer]
+
+        if op == 99:
+            break
+        elif op == 1:
+            cells[cells[pointer+3]] = cells[cells[pointer+1]]+cells[cells[pointer+2]]
+        elif op == 2:
+            cells[cells[pointer+3]] = cells[cells[pointer+1]]*cells[cells[pointer+2]]
+        else:
+            print("Unknown code", op)
+            return None
+
+        pointer += 4
+
+    return cells[0]
+
 
 if __name__ == "__main__":
-    lines = aoc.input.input_lines()
+    cells = [int(a) for a in aoc.input.input_lines()]
+
+    for noun in range(100):
+        for verb in range(100):
+            res = run(cells[:], noun, verb)
+            print(res - 19690720)
+            if res == 19690720:
+                print("Found it!")
+                print(noun)
+                print(verb)
+                exit(0)
 
 
